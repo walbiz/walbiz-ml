0260b2beadf42ac94984e46c81049bd03cd1c798
Muhammad Alif <contact.alif02@gmail.com>
2023-12-18 02:43:09 +0700

    removed limit and offset default values

--- app/controllers/csv_controller.py
+++ app/controllers/csv_controller.py
@@ -3,16 +3,26 @@ from app.utils import read_csv, format_csv_data
 
 app = Flask(__name__)
 
-def paginate_data(data, limit, offset):
-    return data[offset:offset + limit]
+def paginate_data(data, limit=None, offset=None):
+    # Convert limit and offset to integers
+    limit = int(limit) if limit is not None else None
+    offset = int(offset) if offset is not None else None
+
+    # Perform slicing only if both limit and offset are valid integers
+    if limit is not None and offset is not None:
+        return data[offset:offset + limit]
+    elif limit is not None:
+        return data[:limit]
+    else:
+        return data
 
 @app.route('/franchises', methods=['GET'])
 def display_csv():
     csv_data = read_csv('./app/dataset/franchise_walbiz.csv')
     formatted_data = format_csv_data(csv_data)
 
-    limit = int(request.args.get('limit', 10))
-    offset = int(request.args.get('offset', 0))
+    limit = request.args.get('limit')
+    offset = request.args.get('offset')
 
     paginated_data = paginate_data(formatted_data, limit, offset)
     
